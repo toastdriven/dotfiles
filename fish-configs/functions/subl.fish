e09d6c7bc82e520190ec00c3c69919042f0a5c31
Daniel Lindsley <daniel@toastdriven.com>
2021-11-25 20:42:23 -0600

    Added a Sublime CLI command.

--- fish-configs/functions/subl.fish
+++ fish-configs/functions/subl.fish
@@ -1,3 +1,3 @@
 function subl -a path
-    /Users/daniellindsley/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl -l $path
+    /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl -l $path
 end
