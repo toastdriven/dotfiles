393e8abe69b859fef938b92f372d91521645047d
Daniel Lindsley <daniel@toastdriven.com>
2024-08-08 13:07:39 -0500

    Tweaks.

--- fish-configs/config.fish
+++ fish-configs/config.fish
@@ -1,5 +1,11 @@
 # So ssh-agent behaves itself.
-ssh-add ~/.ssh/id_ed25519
+if test -f ~/.ssh/id_rsa
+    ssh-add ~/.ssh/id_rsa
+end
+
+if test -f ~/.ssh/id_ed25519
+    ssh-add ~/.ssh/id_ed25519
+end
 
 # Bump dat ulimit.
 ulimit -n 4096
--- tmux-envs/anthology.sh
+++ tmux-envs/anthology.sh
@@ -7,9 +7,9 @@ tmux new-window -tanthology:1 -n test
 tmux new-window -tanthology:2 -n run
 
 tmux send-keys -tanthology:0 'clear' C-m
-tmux send-keys -tanthology:1 'poetry shell' C-m
-tmux send-keys -tanthology:1 'pytest tests'
-tmux send-keys -tanthology:2 'poetry shell' C-m
+tmux send-keys -tanthology:1 'clear' C-m
+tmux send-keys -tanthology:1 'npm test'
+tmux send-keys -tanthology:2 'clear' C-m
 tmux send-keys -tanthology:2 ''
 
 tmux select-window -tanthology:0
