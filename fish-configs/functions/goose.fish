2ac83ade6746cd06f3844f19be8e089f26b2fd9c
Daniel Lindsley <daniel@toastdriven.com>
2025-11-12 11:51:38 -0600

    Added a goose wrapper that checks for API keys.

--- fish-configs/functions/goose.fish
+++ fish-configs/functions/goose.fish
@@ -4,5 +4,9 @@ function goose --wraps="goose" --description 'goose, but checking API keys first
         return 1
     end
 
+    set api_key_start (string shorten --char="" -m12 "$ANTHROPIC_API_KEY")
+    set api_key_end (string shorten -N -l --char="" -m8 "$ANTHROPIC_API_KEY")
+    echo "Using key '$api_key_start…$api_key_end'."
+
     $HOME/.local/bin/goose $argv;
 end
